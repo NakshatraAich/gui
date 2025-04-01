83d31cd0b86a1f9c2e78c3551f58d7b6b5983d4e
Nakshatra Aich <mounteverest247@gmail.com>
2025-04-01 20:02:40 +0530

    Changed UI - 2

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -40,7 +40,7 @@ export function Home() {
     useEffect(() => {
         const interval = setInterval(() => {
             setCapacity((prev) => (Math.floor(prev > 0 ? prev - 1 : 0)));
-        }, 1000/(100/60)); // Decrease every second
+        }, (25*60*60*1000)/100); // Decrease every second
 
         return () => clearInterval(interval);
     }, []);
@@ -57,50 +57,31 @@ export function Home() {
     const { hrs, mins, secs } = formatTime(timer);
 
     return (
-        <div className="home h-[320px] w-[480px] bg-black grid grid-cols-8 grid-rows-9 p-2 gap-2">
-            <div className="flex flex-col justify-center items-center">
-                <img src={logo} className="h-3 w-auto"/>
+        <div className="home h-[320px] w-[480px] bg-black flex flex-col text-white">
+            <div className="flex flex-row justify-between items-center p-4 border-b border-[#1d1d1d]">
+                <img src={logo} className="h-7 w-auto"/>
+                <div className="custom-font font-semibold text-xl">AL 500</div>
             </div>
 
-            <div className="bg-[#1D1D1D] col-start-1 tracking-wider col-end-5 row-start-2 row-end-10 rounded flex flex-col justify-between items-center">
-                <div className="text-[#626262] font-semibold text-[8px] mt-4">CAPACITY</div>
-                <div className="custom-font font-bold flex-stretch h-full flex flex-col justify-center items-center">
-                    <div className="text-8xl">{capacity}</div>
-                    <div className="text-2xl text-[#B5D411]">%</div>
+            <div className="p-4 mt-8 flex flex-col flex-stretch h-full gap-4">
+                <div className="flex flex-row justify-between items-end"> 
+                    <div className="custom-font text-[100px] leading-[80px] font-bold">{capacity}%</div>
+                    <div className="font-semibold text-xl leading-[15px]">{hrs}:{mins}:{secs}</div>
                 </div>
-                <div className="relative w-full rounded-t overflow-hidden transition-all duration-300 rotate-180">
-                    <div className="h-[20px] bg-[#B5D411] relative" style={{ width: `${capacity}%` }}>                  
-                    </div>
-                </div>
+            
+            <div className="relative p-[10px] w-full">
+                <div className="absolute top-0 right-0 bg-gradient-to-r from-[#B5D411] to-[#8BAA10] h-full" style={{ width: `${capacity}%` }}></div>
+                <div className="absolute top-0 left-0 bg-[#1D1D1D] h-full" style={{ width: `${100 - capacity}%` }}></div>
             </div>
-
-            <div className="border border-[#1D1D1D] tracking-wider col-start-5 col-end-7 row-start-2 row-end-7 rounded flex flex-col justify-between items-center">
-                <div className="text-[#626262] font-semibold text-[8px] mt-4">TIMER</div>
-                <div className="font-bold flex-stretch h-full flex flex-col justify-center items-center">
-                    <div className="text-lg -mt-1">{hrs}</div>
-                    <div className="text-[5px] text-[#626262] -mt-1">HRS</div>
-                    <div className="text-lg -mt-1">{mins}</div>
-                    <div className="text-[5px] text-[#626262] -mt-1">MIN</div>
-                    <div className="text-lg -mt-1">{secs}</div>
-                    <div className="text-[5px] text-[#626262] -mt-1">SEC</div>
+                
+                <div className="font-semibold text-sm leading-[15px]">
+                    OUTPUT: 30W
                 </div>
             </div>
 
-            <div className="border border-[#1D1D1D] col-start-7 col-end-9 row-start-2 row-end-7 rounded flex flex-col justify-between items-center">
-                <div className="text-[#626262] font-semibold text-[8px] mt-4">OUTPUT</div>
-                <div className="font-bold flex-stretch h-full flex flex-col justify-center items-center -mt-4">
-                    <div className="text-4xl -mt-1">100</div>
-                    <div className="text-[5px] text-[#626262]">WATTS</div>
-                </div>
-            </div>
-
-            <div className="custom-font col-start-5 col-end-9 row-start-7 row-end-10 rounded flex flex-col justify-center items-center font-bold text-4xl">
-                <div>AL <span className="text-[#B5D411] tracking-tighter">500</span></div>
-            </div>
-
-            <div className="col-start-1 col-end-9 row-start-10 row-end-11 rounded-sm text-[#626262] flex flex-row w-full justify-between px-1">
-                <div className="text-[9px] font-bold tracking-wider self-center">{dateTime.date.toUpperCase()}</div>
-                <div className="text-[9px] font-bold self-center">{dateTime.time}</div>
+            <div className="col-start-1 col-end-9 row-start-10 row-end-11 rounded-sm text-[#626262] flex flex-row w-full justify-between border-t border-[#1d1d1d] py-2 px-4">
+                <div className="text-[10px] font-bold tracking-wider self-center">{dateTime.date.toUpperCase()}</div>
+                <div className="text-[10px] font-bold self-center">{dateTime.time}</div>
             </div>
         </div>
     );
